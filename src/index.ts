78a67f961c933f677df7ea22a359a59f01b46e87
Chaitanya Narhare <chetannarhare111@gmail.com>
2024-08-02 17:28:12 +0530

    Changes in eslint.json file

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,12 @@ import express from 'express'
 const app = express()
 import dotenv from 'dotenv'
 dotenv.config()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3333
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
 app.listen(port, () => {
-  console.log('hello')
   console.log(`Server is running on http://localhost:${port}`)
 })
